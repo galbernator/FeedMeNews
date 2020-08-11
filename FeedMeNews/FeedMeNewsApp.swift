9403f327a6fa48c7f0bddcf24cfcb8571a8ee13f
Steve Galbraith <steve@stevegalbraith.com>
2020-08-11 16:28:01 -0600

    Initial Commit

---
+++ FeedMeNews/FeedMeNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedMeNewsApp.swift
+//  FeedMeNews
+//
+//  Created by Steve Galbraith on 8/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FeedMeNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
